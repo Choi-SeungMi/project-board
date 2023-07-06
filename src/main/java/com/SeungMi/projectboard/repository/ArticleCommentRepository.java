78438dd38ab89bbcd9312216dfdc8518acdf184d
Choi-SeungMi <115157482+Choi-SeungMi@users.noreply.github.com>
2023-07-07 07:49:11 +0900

    Revert "데이터베이스 접근 로직 테스트 정의"

--- src/main/java/com/SeungMi/projectboard/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SeungMi.projectboard.repository;
-
-import com.SeungMi.projectboard.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
