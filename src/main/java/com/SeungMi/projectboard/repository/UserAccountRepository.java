c1ef865011d628faa56faca1ce333a04d83ef8c8
tmdal8265 <tmdal8265@naver.com>
2023-07-19 15:08:34 +0900

    #34 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/SeungMi/projectboard/repository/UserAccountRepository.java
+++ src/main/java/com/SeungMi/projectboard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.SeungMi.projectboard.repository;
 import com.SeungMi.projectboard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
