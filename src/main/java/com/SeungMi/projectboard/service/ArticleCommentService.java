76ca9fa36333dbd1b4baeb652aeaf254d1801160
tmdal8265 <tmdal8265@naver.com>
2023-07-20 13:29:08 +0900

    #41 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

--- src/main/java/com/SeungMi/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/SeungMi/projectboard/service/ArticleCommentService.java
@@ -1,14 +1,20 @@
 package com.SeungMi.projectboard.service;
 
+import com.SeungMi.projectboard.domain.Article;
+import com.SeungMi.projectboard.domain.ArticleComment;
+import com.SeungMi.projectboard.domain.UserAccount;
 import com.SeungMi.projectboard.dto.ArticleCommentDto;
 import com.SeungMi.projectboard.repository.ArticleCommentRepository;
 import com.SeungMi.projectboard.repository.ArticleRepository;
+import com.SeungMi.projectboard.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -16,18 +22,35 @@ public class ArticleCommentService {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream()
+                .map(ArticleCommentDto::from)
+                .toList();
     }
 
     public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.articleId());
+            UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+            articleCommentRepository.save(dto.toEntity(article, userAccount));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
+        }
     }
 
     public void updateArticleComment(ArticleCommentDto dto) {
-    }
-
+      try {
+        ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+        if (dto.content() != null) { articleComment.setContent(dto.content()); }
+      } catch (EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
+      }
+}
     public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
